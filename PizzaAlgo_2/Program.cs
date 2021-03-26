7cd7b5d72ded8cb49d71687169e4e7c87e0d0bb8
IVProb <jnoojinjr@gmail.com>
2021-03-26 08:36:24 -0500

    initial commit

---
+++ PizzaAlgo_2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PizzaAlgo_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exercise deals with git branching and merging
+
+            // Algo Excercise
+            // 1) Decide what pizzaria to call
+            // 2) Decide if this is pick up or delviery
+            // 3) Decide what toppings to add
+            // 4) Decide what type of crust
+            // 5) Make choice to choose anything extra, like breadsticks or dipping sauce?
+            // 6) How to pay, cash or card?
+            // 7) If card && delivery, would you like to tip the driver?
+        }
+    }
+}
